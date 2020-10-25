b25f17ef5655308a6e65784b9e0c8911b1d73acd
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2020-10-25 14:33:04 +0100

    Use custom image

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ export LOKI_NS="loki"
 minikube start \
 	--memory=4096 \
 	--cpus=2 \
-	--kubernetes-version='v1.16'
+	--vm-driver=hyperkit \
+	--kubernetes-version='v1.16.15'
 
 helm install dicelogger deploy/helm/dicelogger \
     -n "${DICELOGGER_NS}" \
@@ -20,4 +21,4 @@ helm install loki deploy/helm/loki-stack \
 echo -n "---> Admin password: "
 kubectl get secret -n "${LOKI_NS}" loki-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
 
-echo -n "---> Open grafana: kubectl port-forward --namespace ${LOKI_NS} service/loki-grafana 3000:80"
+echo -n "---> Open grafana: kubectl port-forward --namespace ${LOKI_NS} service/loki-grafana 3000:80" ; echo
