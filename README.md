758b27d1b33bcbd4d472a0e4755920243fe53723
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2020-10-25 16:35:32 +0100

    Add grafana dashboard

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 
 
 ## Setup
-
-```bash
-./setup.sh
-```
+- run `setup.sh`, copy GRAFANA_PASSWORD
+- wait for all pods in `loki` namespace to start
+- forward grafana port as printed by the setup script
+- open [grafana](localhost:80)
+- login as `admin:$GRAFANA_PASSWORD`
+- import [dashboard](config/grafana-dashboard.json)
+- explore query `{custom_error_level=~"critical|error|fatal"}`
--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ helm install loki deploy/helm/loki-stack \
     --create-namespace \
     -f config/loki-overrides.yaml
 
-echo -n "---> Admin password: "
+echo -n "---> GRAFANA_PASSWORD: "
 kubectl get secret -n "${LOKI_NS}" loki-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
 
 echo -n "---> Open grafana: kubectl port-forward --namespace ${LOKI_NS} service/loki-grafana 3000:80" ; echo
